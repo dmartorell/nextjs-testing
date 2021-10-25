128c0387cf9537e602a18a2ab2d2ceb3e00a918b
Dani Martorell <64319904+dmartorell@users.noreply.github.com>
2021-10-25 15:06:24 +0200

    Add ESLINT and new rules

--- firebase/client.js
+++ firebase/client.js
@@ -1,50 +1,52 @@
 import { initializeApp } from 'firebase/app';
-import { getAuth, 
-    signInWithPopup, 
-    GithubAuthProvider,
-    GoogleAuthProvider,
-    TwitterAuthProvider,
-    onAuthStateChanged } from "firebase/auth";
+import {
+  getAuth,
+  signInWithPopup,
+  GithubAuthProvider,
+  GoogleAuthProvider,
+  TwitterAuthProvider,
+  onAuthStateChanged,
+} from 'firebase/auth';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyC3IBQF9ZmxcDprwleaGT-n_jtnEakAjLM",
-    authDomain: "ashesapp-616be.firebaseapp.com",
-    projectId: "ashesapp-616be",
-    storageBucket: "ashesapp-616be.appspot.com",
-    messagingSenderId: "622725216039",
-    appId: "1:622725216039:web:8e9d9911df6f5902a51b02"
-  };
+  apiKey: 'AIzaSyC3IBQF9ZmxcDprwleaGT-n_jtnEakAjLM',
+  authDomain: 'ashesapp-616be.firebaseapp.com',
+  projectId: 'ashesapp-616be',
+  storageBucket: 'ashesapp-616be.appspot.com',
+  messagingSenderId: '622725216039',
+  appId: '1:622725216039:web:8e9d9911df6f5902a51b02',
+};
 
 initializeApp(firebaseConfig);
 
 export const loginWithGitHub = () => {
-    const provider = new GithubAuthProvider();
-    const auth = getAuth();
-    return signInWithPopup(auth, provider);
-}
-  
+  const provider = new GithubAuthProvider();
+  const auth = getAuth();
+  return signInWithPopup(auth, provider);
+};
+
 export const loginWithGoogle = () => {
-    const provider = new GoogleAuthProvider();
-    const auth = getAuth();
-    return signInWithPopup(auth, provider);
-}
+  const provider = new GoogleAuthProvider();
+  const auth = getAuth();
+  return signInWithPopup(auth, provider);
+};
 
 export const loginWithTwitter = () => {
-    const provider = new TwitterAuthProvider();
-    const auth = getAuth();
-    return signInWithPopup(auth, provider)
-}
+  const provider = new TwitterAuthProvider();
+  const auth = getAuth();
+  return signInWithPopup(auth, provider);
+};
 
 export const onUserStateChanged = (onChange) => {
-    const auth = getAuth();
-    onAuthStateChanged(auth, result => {
-        if (result){
-            const {
-                displayName : username,
-                email,
-                photoURL : avatar } = result
-                onChange({username, email, avatar})
-        } 
-    });
-}
-
+  const auth = getAuth();
+  onAuthStateChanged(auth, (result) => {
+    if (result) {
+      const {
+        displayName: username,
+        email,
+        photoURL: avatar,
+      } = result;
+      onChange({ username, email, avatar });
+    }
+  });
+};
--- pages/index.js
+++ pages/index.js
@@ -1,68 +1,77 @@
-import {useState, useEffect} from 'react';
-import Button from "../components/Button";
-import GithubLogo from "../components/logos/GitHub";
-import GoogleLogo from "../components/logos/Google";
-import MatchLogo from "../components/logos/Match";
-import TwitterLogo from '../components/logos/Twitter';
+import { useState, useEffect } from 'react';
 import Image from 'next/image';
-import styles from '../styles/Home.module.css'
-import { loginWithGitHub, loginWithGoogle, loginWithTwitter, onUserStateChanged } from "../firebase/client";
-
+import Button from '../components/Button';
+import {
+  GithubLogo, GoogleLogo, MatchLogo, TwitterLogo,
+} from '../components/logos';
+import styles from '../styles/Home.module.css';
+import {
+  loginWithGitHub, loginWithGoogle, loginWithTwitter, onUserStateChanged,
+} from '../firebase/client';
 
 export default function Home() {
-
   const [user, setUser] = useState(null);
-  
+
   const handleGitHubLogin = () => {
     loginWithGitHub()
-      .catch(error => console.log(error.message))
-  }
+      .catch((error) => console.log(error.message));
+  };
   const handleGoogleLogin = () => {
-    loginWithGoogle().catch(error => console.log(error))
-      .catch(error => console.log(error.message))
-
-  }
+    loginWithGoogle().catch((error) => console.log(error))
+      .catch((error) => console.log(error.message));
+  };
   const handleTwitterLogin = () => {
     loginWithTwitter()
-    .catch(error => console.log(error.message))
-  }
+      .catch((error) => console.log(error.message));
+  };
 
-  useEffect(()=> {
-    onUserStateChanged (setUser);
-  }, [])
+  useEffect(() => {
+    onUserStateChanged(setUser);
+  }, []);
 
   return (
     <div>
-        <MatchLogo className={styles.logo} width={95} height={95}/>        
-        <h1 className={styles.title}>
-          Welcome to <span>Grijeen</span>
-        </h1>
-        <h2 className={styles. subTitle}>An app that transform your <span>DREAMS</span> into ashes.</h2>
-        {
-          user ?
-          <div className={styles.userInfoContainer}>
-          <Image className={styles.avatar} width={70} height={70} src={user.avatar} alt="user's profile pic"></Image>
-          <p className={styles.userName}>{user.username}</p>
-          <p className={styles.email}>{user.email}</p>
-        </div>
-        :
-        null
+      <MatchLogo className={styles.logo} width={95} height={95} />
+      <h1 className={styles.title}>
+        Welcome to
+        {' '}
+        <span>Grijeen</span>
+      </h1>
+      <h2 className={styles.subTitle}>
+        An app that transform your
+        {' '}
+        <span>DREAMS</span>
+        {' '}
+        into ashes.
+      </h2>
+      {
+          user
+            ? (
+              <div className={styles.userInfoContainer}>
+                <Image className={styles.avatar} width={70} height={70} src={user.avatar} alt="user's profile pic" />
+                <p className={styles.userName}>{user.username}</p>
+                <p className={styles.email}>{user.email}</p>
+              </div>
+            )
+            : null
         }
-        { user === null && 
+      { user === null
+        && (
         <div className={styles.loginButtonsContainer}>
           <Button onClick={handleGitHubLogin}>
-            <GithubLogo fill="white" width={24} height={24}/>
+            <GithubLogo fill="white" width={24} height={24} />
             Login with GitHub
           </Button>
           <Button onClick={handleGoogleLogin}>
-            <GoogleLogo fill="white" width={24} height={24}/>
+            <GoogleLogo fill="white" width={24} height={24} />
             Login with Google
           </Button>
           <Button onClick={handleTwitterLogin}>
-            <TwitterLogo fill="white" width={24} height={24}/>
+            <TwitterLogo fill="white" width={24} height={24} />
             Login with Twitter
           </Button>
-        </div>}
+        </div>
+        )}
     </div>
-  )
+  );
 }
