ae6780ebc046aaa29ce02531b3ed6fa68cf68a1c
Daniel Martorell <dani.martorell@gmail.com>
2021-11-05 08:07:29 +0100

    Format date when hover on tweet date

--- components/Twitt/index.js
+++ components/Twitt/index.js
@@ -1,10 +1,24 @@
 import useTimeAgo from '../../hooks/useTimeAgo';
 import styles from '../../styles/Twitt.module.css';
 
+const formatCreatedAt = (_createdAt) => {
+  const date = new Date(_createdAt);
+  const options = {
+    year: 'numeric',
+    month: 'short',
+    day: 'numeric',
+    hour: 'numeric',
+    minute: 'numeric',
+    hour12: false,
+  };
+  return new Intl.DateTimeFormat('es-ES', options).format(date);
+};
+
 const Twitt = ({
   avatar, userName, content, createdAt, img,
 }) => {
   const timeAgo = useTimeAgo(createdAt);
+  const formatDate = formatCreatedAt(createdAt);
 
   return (
     <article className={styles.twittContainer}>
@@ -12,7 +26,7 @@ const Twitt = ({
       <div style={{ width: '100%' }}>
         <strong className={styles.twittUsername}>{userName}</strong>
         <span> · </span>
-        <time className={styles.date}>{timeAgo}</time>
+        <time title={formatDate} className={styles.date}>{timeAgo}</time>
         <p className={styles.twittMessage}>{content}</p>
         <div className={styles.imageContainer}>
           <img className={styles.image} src={img} alt={img} />
